826e1bc83331e8164b1a87d107aa46efee7a6e91
Steffen <steffen.schneider02@gmail.com>
2025-05-18 21:50:19 +0200

    Skript hinzugefügt

---
+++ pf.bash
@@ -0,0 +1,13 @@
+
+$n = $1;
+$h = $2;
+
+$dateiname = "processlist_" + date;
+
+if [$h == "h"]
+    then
+        ps -ef > prozessbaum.txt | echo prozessbaum.txt
+    else
+        ps -ef --no-headers > prozessbaum.txt | echo prozessbaum.txt
+
+    
